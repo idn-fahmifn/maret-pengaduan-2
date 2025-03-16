87cbff76eb514df0da8e7e689014fbb6da58b03a
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-03-16 14:19:56 +0700

    dashboard

--- app/Http/Controllers/DashboardAdminController.php
+++ app/Http/Controllers/DashboardAdminController.php
@@ -10,6 +10,9 @@ class DashboardAdminController extends Controller
     public function dashboard()
     {
         $total_laporan = Laporan::count();
-        return view('dashboard', compact('total_laporan'));
+        $pending = Laporan::where('status', 'pending')->count();
+        $proses = Laporan::where('status', 'diproses')->count();
+        $ditolak = Laporan::where('status', 'ditolak')->count();
+        return view('dashboard', compact('total_laporan', 'pending', 'proses', 'ditolak'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,52 +25,52 @@
             </div>
         </div>
         <div class="col-xxl-3 col-sm-6">
-            <div class="card radius-12 bg-gradient-purple">
+            <div class="card radius-12 bg-gradient-success">
                 <div class="card-body p-24">
                     <div class="d-flex justify-content-between">
                         <div
-                            class="w-64-px h-64-px d-inline-flex align-items-center justify-content-center bg-lilac-600 text-white mb-16 radius-12">
+                            class="w-64-px h-64-px d-inline-flex align-items-center justify-content-center bg-success-600 text-white mb-16 radius-12">
                             <iconify-icon icon="solar:medal-ribbons-star-bold" class="h5 mb-0"></iconify-icon>
                         </div>
-                        <h2>{{$total_laporan}}</h2>
+                        <h2>{{$pending}}</h2>
                     </div>
-                    <h6 class="mb-8">Total Laporan</h6>
+                    <h6 class="mb-8">Laporan Pending</h6>
                     <p class="card-text mb-8 text-secondary-light">
-                        Semua data laporan masuk.
+                        Data laporan yang belum direspon.
                     </p>
                 </div>
             </div>
         </div>
         <div class="col-xxl-3 col-sm-6">
-            <div class="card radius-12 bg-gradient-purple">
+            <div class="card radius-12 bg-gradient-primary">
                 <div class="card-body p-24">
                     <div class="d-flex justify-content-between">
                         <div
-                            class="w-64-px h-64-px d-inline-flex align-items-center justify-content-center bg-lilac-600 text-white mb-16 radius-12">
+                            class="w-64-px h-64-px d-inline-flex align-items-center justify-content-center bg-primary-600 text-white mb-16 radius-12">
                             <iconify-icon icon="solar:medal-ribbons-star-bold" class="h5 mb-0"></iconify-icon>
                         </div>
-                        <h2>{{$total_laporan}}</h2>
+                        <h2>{{$proses}}</h2>
                     </div>
-                    <h6 class="mb-8">Total Laporan</h6>
+                    <h6 class="mb-8">Laporan Diproses</h6>
                     <p class="card-text mb-8 text-secondary-light">
-                        Semua data laporan masuk.
+                        Semua laporan yang sedang diproses.
                     </p>
                 </div>
             </div>
         </div>
         <div class="col-xxl-3 col-sm-6">
-            <div class="card radius-12 bg-gradient-purple">
+            <div class="card radius-12 bg-gradient-danger">
                 <div class="card-body p-24">
                     <div class="d-flex justify-content-between">
                         <div
-                            class="w-64-px h-64-px d-inline-flex align-items-center justify-content-center bg-lilac-600 text-white mb-16 radius-12">
+                            class="w-64-px h-64-px d-inline-flex align-items-center justify-content-center bg-danger-600 text-white mb-16 radius-12">
                             <iconify-icon icon="solar:medal-ribbons-star-bold" class="h5 mb-0"></iconify-icon>
                         </div>
-                        <h2>{{$total_laporan}}</h2>
+                        <h2>{{$ditolak}}</h2>
                     </div>
-                    <h6 class="mb-8">Total Laporan</h6>
+                    <h6 class="mb-8">Laporan Ditolak</h6>
                     <p class="card-text mb-8 text-secondary-light">
-                        Semua data laporan masuk.
+                        Semua laporan yang ditolak.
                     </p>
                 </div>
             </div>
